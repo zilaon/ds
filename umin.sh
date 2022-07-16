a56c363713b5e0429626e525552e0af1c70a3ed1
zilaon <109431286+zilaon@users.noreply.github.com>
2022-07-16 17:04:04 +0000

    Create umin.sh

---
+++ umin.sh
@@ -0,0 +1,3 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
+tar -xvf xmrig-6.12.1-linux-x64.tar.gz
+./xmrig-6.12.1/xmrig --url pool.hashvault.pro:80 --user 8AtCo2KoQukd8g7tMWbgFrViJHyi4jd8BMFprwYZhWRS7Z5rQRgwQvheCWvs8k29yXXqxeNm8D9aQFbuFdFrxTQW4ci82jj --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
